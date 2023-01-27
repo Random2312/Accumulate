6882cac088e0dd43c5b8a43bf4570adb82ba48f1
UP <3093077874@qq.com>
2023-01-27 17:40:37 +0800

    优化冒泡排序

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -3,11 +3,36 @@
 
 using namespace std;
 
+//冒泡排序
+void bubble_shot(int *arr,int len)
+{
+	int temp,flag;
+	//外层循环次数为总数-1
+	for (int i = 0; i < 5 - 1; i++)
+	{ 
+		flag = 1;
+		//每轮循环进行比较，比较完的不用再次比较
+		for (int j = 0; j < 5 - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				flag = 0;
+			}
+		//如果数据是有序的，跳出循环
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+
 int main()
 {
-	int temp, i, j, arr[5];
+	int arr[5];
 	//产生随机数
-	for (i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		Sleep(1000);
 		srand((unsigned int)time(NULL));
@@ -15,21 +40,15 @@ int main()
 		cout << arr[i] << endl;
 	}
 
-	//外层循环次数为总数-1
-	for (i = 0; i < 5 - 1; i++)
-		for (j = 0; j < 5 - i - 1; j++)//每轮循环进行比较，比较完的不用再次比较
-			if (arr[j] > arr[j + 1])
-			{
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-	i = 0;
-	while (i < 5)
+	int len = sizeof(arr) / sizeof(arr[0]);
+	bubble_shot(arr, len);
+	cout << "排序后：" << endl;
+
+	for (int i = 0; i < 5; i++)
 	{
-		cout << "排序后：" << arr[i] << endl;
-		i++;
+		cout << arr[i] << endl;
 	}
+
 	system("pause");
 	return 0;
 }
